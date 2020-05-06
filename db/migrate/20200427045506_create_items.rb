d71a5d151a31b41ece92bc76998078b12b6f90f9
d-saori <60836449+d-saori@users.noreply.github.com>
2020-05-06 15:43:16 +0900

    Fix:db/migrate/seeds.rb Errormessage表示の為

--- db/migrate/20200427045506_create_items.rb
+++ db/migrate/20200427045506_create_items.rb
@@ -1,6 +1,8 @@
+# userテーブルに基づくitemのitemテーブル:userに紐づいた商品の情報を表示させるために作成
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      # itemテーブルの構造:itemの名前、値段、user_id(ログインユーザーのデータと紐付けるカラム)
       t.string :name
       t.integer :price
       t.integer :user_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
-Item.create(name: "熱狂のWebマーケティング", price: "1500", user_id: 1)
-Item.create(name: "0秒シンキング", price: "1200", user_id: 1)
-Item.create(name: "死ぬこと以外、けっこう平気。", price: "1300 ", user_id: 1)
+# サンプルとなるデータを登録し、アプリの動作の確認をしやすくする
+# Active Recordのメソッドと一緒。Itemテーブルのcreateメソッドを使う。
+# user_id:全て1だが、これから新しくアプリでデータを投稿する時は、ログインユーザーのidに応じて2、3、4〜という風に番号が振られるようになる
+# create!とするとdbにseedデータ登録時にエラーがあった場合エラーメッセージを表示する
+Item.create!(name: "熱狂のWebマーケティング", price: "1500", user_id: 1)
+Item.create!(name: "0秒シンキング", price: "1200", user_id: 1)
+Item.create!(name: "死ぬこと以外、けっこう平気。", price: "1300 ", user_id: 1)
